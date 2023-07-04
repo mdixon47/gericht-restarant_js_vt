d5af31f54b460f7285e5de245811190f267a5164
Malik Dixon <mdixon47@users.noreply.github.com>
2023-07-04 19:47:49 -0400

    Started Gallery and added readme.md

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,7 +1,16 @@
+// import {BsInstagram, BsArrowLeftShort, BsArrowRightShort} from 'react-icons/bs'
+// import { SubHeading } from '../../components'
+// import { images } from '../../constants'
 import './gallery.css'
 
 const Gallery = () => {
-  return <div>Gallery</div>
+
+
+  return (
+    <div>
+      Gallery
+    </div>
+  )
 }
 
 export default Gallery
